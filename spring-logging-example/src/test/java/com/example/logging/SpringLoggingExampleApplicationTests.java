40fedc52080949874f4729ecccd5010ffa1d2704
namhokim <skaghzz@naver.com>
2021-07-05 17:55:56 +0900

    first commit from spring initializer

---
+++ spring-logging-example/src/test/java/com/example/logging/SpringLoggingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoggingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
